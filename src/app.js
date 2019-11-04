affcba9b51ca59bf61078b4a00ffd6f4a5db8400
Al Clark <alcl@microsoft.com>
2019-11-04 00:31:28 +0000

    Corrected test code

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 http.createServer(function (req, res) {
     fs.readFile("button.html", function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-//        res.write(data);
+//      res.write(data);
         res.write('<div style="text-align: center;">');
         res.write('<h2>CircleCI Button Test</h2>');
         res.write('<button type="button" onClick="getElementById(\'click\').innerHTML = \'You clicked the button!\'">Clicky Button</button>');
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,13 +2,12 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 
 module.exports = {
     'Test button click' : function (browser) {
-        browser
-            .url('http://localhost:8002')
-            .waitForElementVisible('button[type=button]', 5000)
-            .click('button[type=button]'), function(result) {
-                this.assert.strictEqual(result.status, 0)
-            .pause(1000)
-//          .assert.containsText('You clicked the button!')
-            .end();
+        browser.url('http://localhost:8002')
+        browser.waitForElementVisible('button[type=button]', 5000)
+        browser.click('button[type=button]'), function(result) {
+            this.assert.strictEqual(result.status, 0);
+        }
+        browser.expect.element('p').text.to.contain('You clicked the button!')
+        browser.end();
     }
 };
