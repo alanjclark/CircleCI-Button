d4de2de6e9637dec8ab6143b4927bf357c763415
Al Clark <alcl@microsoft.com>
2019-11-05 19:39:11 +0000

    Changed port in app

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,6 @@
 module.exports = {
     'Test button click' : function (browser) {
-        browser.url('http://localhost:8002')
+        browser.url('http://localhost:80')
         browser.waitForElementVisible('button[type=button]', 1000)
         browser.click('button[type=button]'), function(result) {
             this.assert.strictEqual(result.status, 0);
