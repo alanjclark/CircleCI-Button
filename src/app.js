54af371dc00eacc393071426779db4d1bf31fb89
Al Clark <alcl@microsoft.com>
2019-11-05 19:51:58 +0000

    Changing port back to 8002

--- src/app.js
+++ src/app.js
@@ -10,4 +10,4 @@ http.createServer(function (req, res) {
     res.write('<button type="button" onClick="getElementById(\'click\').innerHTML = \'I clicked the button!\'">Clicky Button</button>');
     res.write('<p id="click"></p>');
     res.end();
-}).listen(process.env.PORT || 80);
+}).listen(process.env.PORT || 8002);
